2fd057ba306d4e29f1d07ebc16299ede1c6bc595
amandena <manda.m.johnson@gmail.com>
2021-05-11 21:55:52 -0400

    imported new components and added getInitialLevel function

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,4 +1,19 @@
+import React, { useReducer } from 'react'
+import { LEVELS } from '../levels'
+import { getGameStateFromLevel } from '../core'
+import Level from './Level'
+import Lives from './Lives'
+import Block from './Block'
+import Paddle from './Paddle'
+import Ball from './Ball'
+
 const Scene = (containerSize) => {
+
+  const getInitialLevel = () => {
+    const inState = localStorage.getItem('level')
+    return inState ? parseInt(inState, 10) : 0
+  }
+
   console.log(`${containerSize.width} x ${containerSize.height}`)
   return <svg/>
 }
