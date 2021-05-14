96c203eb1740ee450009847c20f8f6029ea3e46c
amandena <manda.m.johnson@gmail.com>
2021-05-14 17:11:37 -0400

    completed scene component

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -1,11 +1,12 @@
-import React, { useReducer } from 'react'
-import { LEVELS } from '../levels'
-import { getGameStateFromLevel } from '../core'
+import React, { useEffect, useReducer } from 'react'
 import Level from './Level'
 import Lives from './Lives'
 import Block from './Block'
 import Paddle from './Paddle'
 import Ball from './Ball'
+import { LEVELS } from '../levels'
+import { MOVEMENT, getNewGameState, getGameStateFromLevel } from '../core'
+import { registerListener } from '../utils'
 
 const getInitialLevel = () => {
   const inState = localStorage.getItem('level')
@@ -39,11 +40,86 @@ const getInitialState = containerSize => {
   }
 }
 
-const reducer = state => state
+const MOVEMENT_KEYS = {
+  LEFT: [65, 37],
+  RIGHT: [68, 39]
+}
+const STOP_KEY = 32
+const UPDATE_EVERY = 1000 / 60
+const ACTION = {
+  CONTAINER_SIZE_CHANGE: 'CONTAINER_SIZE_CHANGE',
+  KEY_DOWN: 'KEY_DOWN',
+  KEY_UP: 'KEY_UP',
+  TICK: 'TICK'
+}
+const HANDLER = {
+  [ACTION.CONTAINER_SIZE_CHANGE]: (state, containerSize) => ({
+    ...state,
+    containerSize,
+    ...getProjectors(containerSize, state.game.size)
+  }),
+  [ACTION.KEY_DOWN]: (state, key) => {
+    if (MOVEMENT_KEYS.LEFT.includes(key)) {
+      return { ...state, movement: MOVEMENT.LEFT }
+    } else if (MOVEMENT_KEYS.RIGHT.includes(key)) {
+      return { ...state, movement: MOVEMENT.RIGHT }
+    }
+    return state
+  },
+  [ACTION.KEY_UP]: (state, key) => {
+    const newState = { ...state, movement: undefined }
+    if (key === STOP_KEY) {
+      if (state.stopTime) {
+        return { ...newState, stopTime: undefined, time: state.time + Date.now() - state.stopTime}
+      } else {
+        return { ...newState, stopTime: Date.now() }
+      }
+    }
+    return newState
+  },
+  [ACTION.TICK]: state => {
+    if (state.stopTime) return state
+    const time = Date.now()
+    const newGame = getNewGameState(state.game, state.movement, time - state.time)
+    const newState = { ...state, time }
+    if (newGame.lives < 1) {
+      return { ...newState, game: getGameStateFromLevel(LEVELS[state.level]) }
+    } else if (newGame.blocks.length < 1) {
+      const level = state.level === LEVELS.length ? state.level : state.level + 1
+      localStorage.setItem('level', level)
+      const game = getGameStateFromLevel(LEVELS[state.level])
+      return {
+        ...newState,
+        level,
+        game,
+        ...getProjectors(state.containerSize, game.size)
+      }
+    }
+    return { ...newState, game: newGame }
+  }
+}
 
 const Scene = (containerSize) => {
 
-  const [state] = useReducer(reducer, containerSize, getInitialState)
+  const [state, dispatch] = useReducer(reducer, containerSize, getInitialState)
+  const act = (type, payload) => dispatch({ type, payload })
+
+  useEffect(() => act(ACTION.CONTAINER_SIZE_CHANGE, containerSize), [containerSize])
+  useEffect(() => {
+    const onKeyDown = ({ which }) => act(ACTION.KEY_DOWN, which)
+    const onKeyUp = ({ which }) => act(ACTION.KEY_UP, which)
+    const tick = () => act(ACTION.TICK)
+
+    const timerId = setInterval(tick, UPDATE_EVERY)
+    const unregisterKeydown = registerListener('keydown', onKeyDown)
+    const unregisterKeyup = registerListener('keyup', onKeyUp)
+    return () => {
+      clearInterval(timerId)
+      unregisterKeydown()
+      unregisterKeyup()
+    }
+  }, [])
+  
   const {
     projectDistance,
     projectVector,
@@ -83,6 +159,12 @@ const Scene = (containerSize) => {
       <Ball {...projectVector(ball.center)} radius={unit} />
     </svg>
   )
+}
+
+const reducer = (state, { type, payload }) => {
+  const handler = HANDLER[type]
+  if (!handler) return state
+  return handler(state, payload)
 }
 
 export default Scene
